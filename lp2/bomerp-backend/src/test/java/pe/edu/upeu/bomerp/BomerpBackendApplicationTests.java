314a38bebd2359b02a286ea67f3fd79593083f63
unknown <heliofaicali@gmail.com>
2026-08-23 01:25:26 -0500

    test: configurar H2 en tests unitarios para BUILD SUCCESS completo (7/7 tests)

--- lp2/bomerp-backend/src/test/java/pe/edu/upeu/bomerp/BomerpBackendApplicationTests.java
+++ lp2/bomerp-backend/src/test/java/pe/edu/upeu/bomerp/BomerpBackendApplicationTests.java
@@ -2,10 +2,15 @@ package pe.edu.upeu.bomerp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("dev")
+@SpringBootTest(properties = {
+	"spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;MODE=Oracle",
+	"spring.datasource.driver-class-name=org.h2.Driver",
+	"spring.datasource.username=sa",
+	"spring.datasource.password=",
+	"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+	"spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class BomerpBackendApplicationTests {
 
 	@Test
@@ -13,4 +18,5 @@ class BomerpBackendApplicationTests {
 	}
 
 }
+
 
